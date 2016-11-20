65a48873979096f40bdd36f29f9df4ece017b849
Dominic Canora <dominiccanora@gmail.com>
2016-11-20 13:38:39 -0500

    Grade select is working.

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 var c = document.getElementById("canv");
 var x = c.getContext("2d");
 var worker = new Worker("worker.js");
+var isGradeInit = false;
+var grade;
+var initInterval;
+
+var initCanvas = function () {
+    resizeCanvas();
+    x.font = "120px serif";
+    x.textAlign = "center";
+    wheelMe();
+    x.fillStyle = "#000000";
+    x.fillText("9", c.width * 0.2, c.height * 0.5);
+    x.fillText("10", c.width * 0.4, c.height * 0.5);
+    x.fillText("11", c.width * 0.6, c.height * 0.5);
+    x.fillText("12", c.width * 0.8, c.height * 0.5);
+};
 
 var resizeCanvas = function () {
     c.width = window.innerWidth;
-    c.height = window.innerHeight
+    c.height = window.innerHeight;
 };
 
 window.addEventListener("resize", resizeCanvas);
-window.addEventListener("load", resizeCanvas);
-
-worker.postMessage("hayyy");
+window.addEventListener("load", function () {
+    initInterval = setInterval(initCanvas, 10);
+});
+c.addEventListener("mousedown", function (e) { //On click grade
+    onClickGrade(e);
+});
 
 worker.onmessage = function (event) {
     data = event.data;
@@ -18,3 +36,48 @@ worker.onmessage = function (event) {
     x.fillRect(0, 0, c.width, c.height);
     console.log("Message from worker! " + data)
 };
+
+var onClickGrade = function (e) {
+    var xProp = e.clientX / c.width;
+    if(xProp < 0.1 || xProp > 0.9) return;
+    console.log("Xprop is " + xProp);
+    clearInterval(initInterval);
+    if(xProp > 0.1 && xProp < 0.3) {grade = 9; x.fillStyle = "#ff0000";}
+    if(xProp > 0.3 && xProp < 0.5) {grade = 10; x.fillStyle = "#00ffff";}
+    if(xProp > 0.5 && xProp < 0.7) {grade = 11; x.fillStyle = "#23f300";}
+    if(xProp > 0.7 && xProp < 0.9) {grade = 12; x.fillStyle = "#808080";}
+    x.fillRect(0, 0, c.width, c.height);
+    x.fillStyle = "#000000";
+    x.fillText(grade, c.width/2, c.height/2);
+    isGradeInit = true;
+    console.log("Grade is " + grade);
+};
+
+var wheelMe = function () {
+    var time = (new Date).getTime();
+    time /= 100;
+    time = Math.round(time);
+    x.fillStyle = wheel(time % 255);
+    x.fillRect(0, 0, c.width, c.height)
+};
+
+function wheel(pos) {
+    pos = 255 - pos;
+    if(pos < 85) {
+        return rgbToHex(255 - pos * 3, 0, pos * 3);
+    }
+    if(pos < 170) {
+        pos -= 85;
+        return rgbToHex(0, pos * 3, 255 - pos * 3);
+    }
+    pos -= 170;
+    return rgbToHex(pos * 3, 255 - pos * 3, 0);
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
